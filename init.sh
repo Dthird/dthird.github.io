33f90ee744bd1f7e75a05c08dee33a5a3955902c
Jiaxing Dong <sydongjx@163.com>
2016-09-22 21:03:24 +0800

    Add articles

--- init.sh
+++ init.sh
@@ -2,5 +2,7 @@
 
 echo "npm install hexo --save"
 npm install hexo --save
+echo "npm install"
+npm install
 echo "npm install hexo-deployer-git --save"
 npm install hexo-deployer-git --save
---
+++ source/_posts/TESTSSSSSS.md
@@ -0,0 +1,6 @@
+---
+title: TESTSSSSSS
+date: 2016-09-22 21:03:02
+categories:
+tags:
+---
