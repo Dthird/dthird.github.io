7d5ad48591289d938aee3c9a9ec58d61256af91e
Jiaxing Dong <sydongjx@163.com>
2016-09-22 19:00:37 +0800

    Add articles

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "hexo clean"
+hexo clean
+echo "hexo g"
+hexo g
+echo "hexo d"
+hexo d
+
+echo "git add ."
+git add .
+echo "git commit"
+git commit -m "Add articles"
+echo "git push origin master"
+git push origin master
